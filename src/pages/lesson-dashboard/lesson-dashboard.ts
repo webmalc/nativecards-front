8f1e7a078509958b00df6bf271e6cb6152826089
webmalc <m@webmalc.pw>
2018-10-17 12:42:42 +0300

    complete a page with a lesson's dashboard

--- src/pages/lesson-dashboard/lesson-dashboard.ts
+++ src/pages/lesson-dashboard/lesson-dashboard.ts
@@ -7,6 +7,7 @@ import { BasePage } from '../../lib/page';
 import { Query } from '../../models/query';
 import { Deck } from '../../models/deck';
 import { Card } from '../../models/card';
+import { WordDisplayPage } from '../word-display/word-display';
 
 
 @IonicPage()
@@ -19,6 +20,8 @@ export class LessonDashboardPage extends BasePage {
   public decks: Array<Deck>;
 
   public cards: Array<Card>;
+
+  public distinctCards: Array<Card>;
 
   public query: Query;
 
@@ -41,7 +44,7 @@ export class LessonDashboardPage extends BasePage {
     }
   }
 
-  public ionViewWillEnter() {
+  public ionViewWillLoad() {
     this.decksProvider.fetchEnabled().subscribe(decks => {
       this.decks = decks.results;
     }, error => {
@@ -50,13 +53,38 @@ export class LessonDashboardPage extends BasePage {
     this.loadLesson();
   }
 
-  public loadLesson() {
+  private _distinct(cards: Array<Card>): Array<Card> {
+    let words = [];
+    return cards.filter(card => {
+      if (words.indexOf(card.word) === -1) {
+        words.push(card.word);
+        return true;
+      }
+      return false;
+    }).sort((a, b) => {
+      if (a.word < b.word) {
+        return -1;
+      } else if (a.word > b.word) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
 
+  // Display the word
+  public display(card: Card) {
+    this.navCtrl.push(WordDisplayPage, { 'card': card });
+  }
+
+  public loadLesson() {
     this.cards = null;
     this.isSearching = true;
+    this.distinctCards = null
     this.cardsProvider.lessons(this.query).subscribe(cards => {
       this.isSearching = false;
       this.cards = cards;
+      this.distinctCards = this._distinct(cards);
     }, error => {
       this.isSearching = false;
       this.showMessage()
